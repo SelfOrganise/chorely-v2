34ac9b27c0054e6f77f988135be4b98302a3c401
Bogdan B <889997+bogdbo@users.noreply.github.com>
2024-01-10 23:34:07 +0000

    fix: auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,19 @@
 import { default as nextAuthMiddleware, WithAuthArgs } from 'next-auth/middleware';
 import { NextRequest } from 'next/server';
 
-export default function middleware(...args: WithAuthArgs): unknown {
-  if (args[0] instanceof NextRequest) {
+export default function middleware(...args: any): unknown {
+  if (typeof args[0]?.nextUrl?.pathname === 'string') {
     const request: NextRequest = args[0];
 
-    const publicPaths = ['/auth', '/secret/signup', '/api/trpc', '/api/auth', '/manifest.json', '/favicon', '/chores.png']; // trpc handles api/trpc
+    const publicPaths = [
+      '/auth',
+      '/secret/signup',
+      '/api/trpc',
+      '/api/auth',
+      '/manifest.json',
+      '/favicon',
+      '/chores.png',
+    ]; // trpc handles api/trpc
 
     if (publicPaths.some(path => request.nextUrl.pathname.startsWith(path))) {
       return;
